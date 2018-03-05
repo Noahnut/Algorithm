5d8f1ab736da9babba38e7576a24f06c85624f9f
Noahnut <b02204048@ntu.edu.tw>
2018-03-05 20:25:35 +0800

    Create main.cpp

---
+++ sort/heapsort/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include "heapsort.hpp"
+
+using namespace std;
+
+//class declaration 
+heap_tree heapTree;
+Test_data test;
+
+int main()
+{
+  int range_number;
+  cout << "enter the range number" << endl;
+  cin >> range_number;
+  vector<size_t> number;
+  number = test.RamdomNumberGen(range_number);
+  heapTree.Init_Heap_Tree(range_number, number);
+  test.display_test();
+  heapTree.display();
+  heapTree.sort();
+  heapTree.display_sort();
+  return 0;
